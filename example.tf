2da2fa3a2f38e73fbb7f0f352733321f62b0fb78
sanatcodehub <sanu97300@gmail.com>
2024-08-06 17:33:31 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,14 +1,20 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA4AJCU34ANEILXAHH"
-  secret_key = "XwE0HpvzkMQ8imJusgrgvRB6j9qYDD+V04KIXNH/"
+  region = "us-east-1"
 }
 
-# resource
-resource "aws_instance" "mywebserver" {
-  ami = "ami-0c7217cdde317cfec"
+resource "aws_instance" "example" {
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
-  key_name = "n.virginia_FEB2024"
-  security_groups = ["launch-wizard-1"]  # Consider using a custom security group
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sannatt"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
